77547f2726e5b60cd826e4eebd2b3eab9a517428
Ashok Kumar <ashokkumar56624@gmail.com>
2021-10-20 01:23:53 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,36 +7,38 @@ import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import Footer from './components/Footer/Footer';
 import './App.css';
- 
+
 const particlesOptions = {
   particles: {
     number: {
       value: 30,
       density: {
         enable: true,
-        value_area: 300
+        value_area:800
       }
     }
   }
 }
 
 const initialState = {
-  input: '',
-  imageUrl: '',
-  box: {},
-  route: 'signin',
-  isSignedIn: false,
-  user: {
-    id: '',
-    name: '',
-    email: '',
-    entries: 0,
-    joined: ''
-  }
-}
+        input: '',
+        imageUrl: '',
+        box: {},
+        route: 'signin',
+        isSignedIn: false,
+        user: {
+          id: '',
+          name: '',
+          email: '',
+          entries: 0,
+          joined: ''
+         }
+    }
 
 class App extends Component {
+
   constructor() {
     super();
     this.state = initialState;
@@ -44,45 +46,46 @@ class App extends Component {
 
   loadUser = (data) => {
     this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined
     }})
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
+   const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+   const image = document.getElementById('inputImage');
+   const width = Number(image.width);
+   const height = Number(image.height);
+   return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
       rightCol: width - (clarifaiFace.right_col * width),
       bottomRow: height - (clarifaiFace.bottom_row * height)
+
     }
   }
 
-  displayFaceBox = (box) => {
-    this.setState({box: box});
+   displayFaceBox = (box) => {
+    this.setState({ box: box });
   }
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
+    this.setState({ input: event.target.value });
   }
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('https://myfacedetection-app.herokuapp.com/imageurl', {
-        method: 'post',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          input: this.state.input
-        })
-      })
-      .then(response => response.json())
+    fetch('https://myfacedetection-app.herokuapp.com/imageurl', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              input: this.state.input
+            })
+          })
+    .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('https://myfacedetection-app.herokuapp.com/image', {
@@ -92,20 +95,19 @@ class App extends Component {
               id: this.state.user.id
             })
           })
-            .then(response => response.json())
-            .then(count => {
+          .then(response => response.json())
+          .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
-            .catch(console.log)
-
+          .catch(console.log)
         }
-        this.displayFaceBox(this.calculateFaceLocation(response))
+        this.displayFaceBox( this.calculateFaceLocation(response)) 
       })
       .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
-    if (route === 'signout') {
+    if (route === 'signout'){
       this.setState(initialState)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
@@ -113,33 +115,30 @@ class App extends Component {
     this.setState({route: route});
   }
 
-  render() {
-    const { isSignedIn, imageUrl, route, box } = this.state;
+    render() {
+     const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
-         <Particles className='particles'
-          params={particlesOptions}
+        <Particles className='particles' 
+                params={particlesOptions}
         />
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
-        { route === 'home'
-          ? <div>
-              <Logo />
-              <Rank
-                name={this.state.user.name}
-                entries={this.state.user.entries}
-              />
-              <ImageLinkForm
-                onInputChange={this.onInputChange}
-                onButtonSubmit={this.onButtonSubmit}
-              />
-              <FaceRecognition box={box} imageUrl={imageUrl} />
-            </div>
-          : (
-             route === 'signin'
-             ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-             : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-            )
-        }
+
+              <Navigation isSignedIn = {isSignedIn} onRouteChange = {this.onRouteChange} />
+              { route === 'home' 
+              ? <div>
+                    <Logo />
+                    <Rank name = {this.state.user.name} entries = {this.state.user.entries}/>
+                    <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit = {this.onButtonSubmit} />
+                    <FaceRecognition box={box} imageUrl={imageUrl}/>
+                </div>
+                : (
+                    route === 'signin' 
+                    ? <Signin loadUser = {this.loadUser} onRouteChange = {this.onRouteChange} />
+                    : <Register loadUser = {this.loadUser} onRouteChange = {this.onRouteChange} />
+                  )
+               }
+               <Footer onRouteChange = {this.onRouteChange} />
+      
       </div>
     );
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = ({onRouteChange}) => {
+	return (
+		<div>
+			<footer>
+			<p className='pa3'> All Rights Reserved &copy; 2019 <a className='black' href='https://github.com/shubhamd99/shubhamd99.github.io'>Shubham Dhage.</a> </p>
+			</footer>
+		</div>
+	);
+}
+
+
+export default Footer;
